95affcb6342b513654e814740066abe0851e0781
BitPulse01 <bitpulse0101@gmail.com>
2024-08-11 12:48:33 +0330

    Reading Headers and bug fixes

--- app/main.js
+++ app/main.js
@@ -7,35 +7,47 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
     socket.on('data', (data) => {
-        let message = "HTTP/1.1 200 OK\r\n\r\n";
-        
-        data = data.toString().split("\r\n")[0].split(" ");
-        
-        const ENDPOINTS = ["/", "/index.html"];
+        let Request = data.toString().split("\r\n")
+        let Request_Info = Request[0].split(" ");
+        let Request_Header = Request[1].split(": ");
         
         let RequestData = {
             "method":data[0],
             "url_path":data[1],
-            "HTTP_version":data[2]
+            "HTTP_version":data[2],
+            "User-agent":Request_Header[1],
         }
         
-        if (ENDPOINTS.includes(RequestData['url_path'])) {
-            message = "HTTP/1.1 200 OK\r\n\r\n";
-        }
-        
-        if (RequestData['url_path'].toString().startsWith("/echo/")){
+        if (RequestData['url_path'] == "/") {
+            let message = "HTTP/1.1 200 OK\r\n\r\n";
+            
+            console.log(message);
+            socket.write(message);
+
+        } else if (RequestData['url_path'].toString().includes("/echo/")){
             let echoMessage = RequestData['url_path'].toString().split("/echo/")[1].toString();
             
-            message = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echoMessage.length}\r\n\r\n${echoMessage}`
-        }
+            let message = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echoMessage.length}\r\n\r\n${echoMessage}`
 
-        else{
-            message = "HTTP/1.1 404 Not Found\r\n\r\n";
+            console.log(message);
+            socket.write(message);
+
+        } else if (RequestData['url_path'] == "/user-agent"){
+            let UserAgent = RequestData['User-agent'];
+            let message = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${UserAgent.length}\r\n\r\n${UserAgent}`;
+
+
+            console.log(message);
+            socket.write(message);
+
+        } else{
+            let message = "HTTP/1.1 404 Not Found\r\n\r\n";
+            
+            console.log(message);
+            socket.write(message);
         }
-        
+       
         console.log(RequestData);
-        console.log(message);
-        socket.write(message);
     });
 
     socket.on("close", () => {
